da8b928084c44ab9b40aad8aea116c4260b352cb
Phuoc Pham <phamhbphuoc@gmail.com>
2026-01-09 01:20:20 -0500

    added review api

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -67,16 +67,31 @@ const isValid = (username)=>{ //returns boolean
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
     const book = books[req.params.isbn];
-    const reviews = book.reviews;
-    const username = req.body.username;
-    const review = req.body.review
-    reviews[username] = review;
-    // if (username in reviews) {
-    //     reviews[username] = review;
-    // } else {
-    //     reviews[username] = review;
-    // }
-    return res.json(review)
+    if (book) {
+        const reviews = book.reviews;
+        const review = req.query.review;
+        const username = req.session.authorization.username;
+        reviews[username] = review;
+        return res.status(200).json({message: "Review added/updated successfully"})
+    } else {
+        return res.status(404).json({message: "Book not found"})
+    }
+
+});
+
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const book = books[req.params.isbn];
+    if (book) {
+        const reviews = book.reviews;
+        const username = req.session.authorization.username;
+        delete reviews[username];
+        return res.status(200).json({message: "Review deleted successfully"})
+    } else {
+        return res.status(404).json({message: "Book not found"})
+    }
+
 });
 
 module.exports.authenticated = regd_users;
